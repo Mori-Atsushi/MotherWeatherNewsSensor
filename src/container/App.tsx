bddcab5f6c3bb6ebb0f3d7b15fe334e09168035b
Mori Atsushi <mori@cyder.tech>
2018-08-24 01:47:48 +0900

    起動中を保存するようにした

--- src/container/App.tsx
+++ src/container/App.tsx
@@ -1,15 +1,39 @@
 import * as React from 'react';
+import styled from 'styled-components';
 
 import CameraPreview from '../component/CameraPreview';
 import SpearchRecognizer from '../component/SpearchRecognizer';
 
-export default class App extends React.Component {
+interface IState {
+  isRuning: boolean
+}
+
+export default class App extends React.Component<{}, IState> {
+  constructor() {
+    super({});
+    this.state = ({
+      isRuning: false
+    });
+    this._handleClick = this._handleClick.bind(this);
+  }
+
   public render() {
     return (
       <div>
+        <Button onClick={this._handleClick}>
+          { this.state.isRuning ? 'Stop' : 'Start'}
+        </Button>
         <CameraPreview />
         <SpearchRecognizer />
       </div>
     );
   }
+
+  private _handleClick(e: React.MouseEvent<HTMLButtonElement>) {
+    this.setState({ isRuning: !this.state.isRuning })
+  }
 }
+
+const Button = styled.button`
+  margin: 10px;
+`;
